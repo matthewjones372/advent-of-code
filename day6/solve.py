fc579966b4fa60a0d727c3fa663d0f30358cccd3
matt <matthewjones372@gmail.com>
2020-12-06 19:48:07 +0000

    Day 6

---
+++ day6/solve.py
@@ -0,0 +1,21 @@
+from itertools import chain
+from collections import Counter
+
+
+def read_file(filename: str = "question_answers.txt"):
+    with open(filename) as f:
+        return [group.strip().split() for group in f.read().split("\n\n")]
+
+
+def count_yes(group_answers):
+    return [len(set(chain(*group))) for group in group_answers]
+
+
+def all_yes(group_answers):
+    return sum([Counter(list(Counter("".join(group)).values()))[len(group)] for group in group_answers])
+
+
+if __name__ == '__main__':
+    group_answers = read_file()
+    print(f"Part 1 Solution : {sum(count_yes(group_answers))}")
+    print(f"Part 2 Solution : {all_yes(group_answers)}")
